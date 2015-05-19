e79c16bbcc1c14ea6cd9a2e2af5d8f286cf84f84
Serge Balyuk <bgipsy@gmail.com>
2015-05-20 00:42:44 +0300

    Adds lightweight ruby methods and aliases

--- lib/byte_buffer/buffer.rb
+++ lib/byte_buffer/buffer.rb
@@ -2,5 +2,24 @@ module ByteBuffer
   class Buffer
     alias_method :size, :length
     alias_method :bytesize, :length
+    alias_method :<<, :append
+    alias_method :cheap_peek, :to_str
+
+    def empty?
+      self.length == 0
+    end
+
+    def eql?(other)
+      self.to_str.eql?(other.to_str)
+    end
+    alias_method :==, :eql?
+
+    def hash
+      to_str.hash
+    end
+
+    def dup
+      self.class.new(self.to_str)
+    end
   end
 end
