2c743f1ccace25f2ca13144a4898f8c1f59ed27c
Serge Balyuk <bgipsy@gmail.com>
2015-05-19 17:40:50 +0300

    Adds bolierplate for data bag management

--- ext/byte_buffer_ext/byte_buffer.c
+++ ext/byte_buffer_ext/byte_buffer.c
@@ -5,12 +5,73 @@
  */
 
 #include "ruby.h"
+#include <string.h>
+
+static VALUE rb_byte_buffer_allocate(VALUE klass);
+static VALUE rb_byte_buffer_lol_int(VALUE self);
+static VALUE rb_byte_buffer_lol_str(VALUE self);
+
+static void byte_buffer_free(void *ptr);
+static size_t byte_buffer_memsize(const void *ptr);
+
+static const rb_data_type_t buffer_data_type = {
+    "byte_buffer/buffer",
+    {NULL, byte_buffer_free, byte_buffer_memsize}
+};
+
+typedef struct {
+    size_t size;
+    size_t read_pos;
+    char   embedded_buffer[1024];
+} buffer_t;
 
 void
 Init_byte_buffer_ext()
 {
-    VALUE mByteBuffer, cBuffer;
+    VALUE rb_mByteBuffer, rb_cBuffer;
 
-    mByteBuffer  = rb_define_module_under(rb_cObject, "ByteBuffer");
-    cBuffer      = rb_define_class_under(mByteBuffer, "BufferLol", rb_cObject);
+    rb_mByteBuffer  = rb_define_module("ByteBuffer");
+    rb_cBuffer      = rb_define_class_under(rb_mByteBuffer, "BufferLol", rb_cObject);
+
+    rb_define_alloc_func(rb_cBuffer, rb_byte_buffer_allocate);
+    rb_define_method(rb_cBuffer, "lol_int", rb_byte_buffer_lol_int, 0);
+    rb_define_method(rb_cBuffer, "lol_str", rb_byte_buffer_lol_str, 0);
+}
+
+VALUE
+rb_byte_buffer_allocate(VALUE klass)
+{
+    buffer_t *b;
+    VALUE obj = TypedData_Make_Struct(klass, buffer_t, &buffer_data_type, b);
+    memcpy(b->embedded_buffer, "wee-haa", strlen("wee-haa"));
+
+    return obj;
+}
+
+VALUE
+rb_byte_buffer_lol_int(VALUE self)
+{
+    return INT2NUM(42);
+}
+
+VALUE
+rb_byte_buffer_lol_str(VALUE self)
+{
+    buffer_t *b;
+    TypedData_Get_Struct(self, buffer_t, &buffer_data_type, b);
+
+    return rb_str_new_cstr(b->embedded_buffer);
+}
+
+void
+byte_buffer_free(void *ptr)
+{
+    buffer_t *b = ptr;
+    xfree(b);
+}
+
+size_t
+byte_buffer_memsize(const void *ptr)
+{
+    return ptr ? sizeof(buffer_t) : 0;
 }
